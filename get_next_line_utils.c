feb09d3f8713c178010321e0a2aa75ec3d7532df
Gregoire Beaumann <gbeauman@c1r4s4.42lausanne.ch>
2021-12-22 16:35:22 +0100

    mort

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: gbeauman <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 12:38:15 by gbeauman          #+#    #+#             */
-/*   Updated: 2021/12/21 17:14:51 by gbeauman         ###   ########.fr       */
+/*   Updated: 2021/12/22 16:34:22 by gbeauman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include	"get_next_line.h"
@@ -50,12 +50,36 @@ static char *ft_join(char *pre, char *post, int len)
 		str[i] = post[i];
 		i++;
 	}
+	if (str[i - 1] == '\n')
+	{
+		str[i] = '\0';
+		i--;
+	}
+	if (pre[i2] == '\n')
+		i2++;
 	while (pre[i2])
 	{
 		str[i + i2] = pre[i2];
 		i2++;
 	}
+	str[i + i2] = '\0';
 	return (str);
+}
+
+static char	isretligne(char *stock)
+{
+	int	i;
+
+	i = 0;
+//	printf ("%s", stock); //test
+	if (!stock)
+		return (0);
+	while (stock[i])
+		i++;
+	i--;
+//	write (1, &stock[i], 1); //test
+//	write (1, "\n", 1); //test
+	return (*&stock[i]);
 }
 
 void	ft_separation(char *buffer)
@@ -65,25 +89,31 @@ void	ft_separation(char *buffer)
 	char	*pre;
 	static char	*post;
 	char	*combi;
+	static char	*stock;
 
-	pre = (char *) malloc(prelen(buffer) + 1 * sizeof(char));
-	while (buffer[i] && buffer[i] != '\n')
+	pre = (char *) malloc(prelen(buffer) * sizeof(char));
+	stock = post;
+//	printf ("%s\n", stock); // test
+	post = (char *) malloc(postlen(buffer, i) * sizeof(char));
+	if (isretligne(stock) == '\n')
+		*pre = '\0';
+	else
 	{
-		pre[i] = buffer[i];
+		while (buffer[i] && buffer[i] != '\n')
+		{
+			pre[i] = buffer[i];
+			i++;
+		}
 		i++;
 	}
-	pre[i] = '\0';
-	i++;
-	post = (char *) malloc(postlen(buffer, i) + 1 * sizeof(char));
-	combi = (char *)malloc((prelen(buffer) + 1 + postlen(buffer, i) + 1 * sizeof(char)));
-	combi = ft_join(pre, post, (prelen(buffer) + postlen(buffer, i)));
+	combi = (char *)malloc((prelen(buffer)  + postlen(buffer, i)  * sizeof(char)));
+	combi = ft_join(pre, stock, (prelen(buffer) + postlen(buffer, i)));
 	printf("%s\n", combi);
-	while (buffer[i] != '\0' && buffer[i] != '\n')
+	while (buffer[i] != '\0' && (buffer[i] != '\n' + 1))
 	{
 		post[i2] = buffer[i];
 		i++;
 		i2++;
 	}
-	post[i2] = '\0';
-	printf ("%s\n" , post);
+//	printf ("%s", post);
 }
